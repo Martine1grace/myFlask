ff32485ecaebcab23319dd799996ddc3014d4625
martine1grace <martinegrace16@gmail.com>
2022-11-03 18:16:45 +0200

    date,contact,birthdaytime page

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask,request
+from datetime import datetime
+
+app=Flask('my first application')
+
+@app.route('/')
+def index():
+    return """
+    <h1> NLP Fellowship</h1>
+    <h2>My Name is Martine Grace</h2>
+    <p> Questions, comments or improvements? Please create an issue on Github, tweet at @corydolphin or send me an email.
+     I do my best to include every contribution proposed in any way that I can.</p>"""
+
+@app.route('/contacts')
+def contacts():
+    return 'do you need my contact panda'
+@app.route('/date')
+def datepage():
+    date=str(datetime.now())
+    return f'to day is {date}' 
+@app.route('/birthdaytime',methods=['GET','POST'])
+def calc_brithdaytime():
+    if request.method == 'POST':
+        return f"from   your submission  age is {2022 - int(request.form.get('birthdaytime'))}"
+    elif request.method == 'GET':
+        return"""
+        <form method='POST' action='/birthdaytime'>
+           <input type="number" name='birthdaytime' placeholder="number of ages"/>
+          <input type="submit" value="submit" />
+        </form>"""
+
+if __name__ == '__main__':
+        app.run()
+
+ 
